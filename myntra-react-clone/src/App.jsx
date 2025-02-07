27dfaf31f1c475388299f13b4ffcb18b26ad4523
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-02-07 15:53:49 +0530

    Initial Commit

---
+++ myntra-react-clone/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="items-container"></div>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
